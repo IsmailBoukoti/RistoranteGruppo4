3fbc27d635265fb7e12ec317a0880b27bfd79332
michelepc <mangiacotti.fi@gmail.com>
2023-01-16 14:19:07 +0100

    Code review 16_01_2023

--- src/it/restaurant/RestaurantTableReservation/TableStateEnum.java
+++ src/it/restaurant/RestaurantTableReservation/TableStateEnum.java
@@ -1,6 +1,8 @@
 package it.restaurant.RestaurantTableReservation;
 
 public enum TableStateEnum {
+
+    //TODO inserire altri parametri che possono essere utili, di sicuro la forma stringata ci serve per poi stampare operazioni
     FREE,
     OCCUPIED,
 }
